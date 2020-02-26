e2289157114abd3ef5e526c56684a72d5457314e
AngelVil92 <angelvil92@gmail.com>
2020-02-26 15:37:00 +0100

    Computed prop

--- proyecto/src/App.vue
+++ proyecto/src/App.vue
@@ -4,17 +4,21 @@
     <!-- <p>Me llamo {{nombre}}</p> -->
     <!-- <app-contador></app-contador> -->
     <!-- <cmp-global></cmp-global> -->
-    <cmp-directivas></cmp-directivas>
+    <!-- <cmp-directivas></cmp-directivas> -->
+    <cmp-otras-opciones></cmp-otras-opciones>
   </div>
 </template>
 
 <script>
 import Contador from './components/Contador.vue';
 import CmpDirectivas from './components/directivas/CmpDirectivas.vue';
+import CmpOtrasOpciones from './components/CmpOtrasOpciones.vue';
+
 export default {
   components: {
     'app-contador': Contador,
-    'cmp-directivas': CmpDirectivas
+    'cmp-directivas': CmpDirectivas,
+    'cmp-otras-opciones': CmpOtrasOpciones
   },
   data() {
     return {
---
+++ proyecto/src/components/CmpOtrasOpciones.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{nombre}} {{apellido}}</p>
+    <p>{{nombreCompleto}}</p>
+    <input type="text" v-model="nombre">
+    <input type="text" v-model="apellido">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: '',
+      apellido: '',
+      precio: 4,
+      msg: 'Validando...'
+    }
+  },
+  computed: {
+    nombreCompleto() {
+      // console.log(this.$data);
+      return this.nombre + ' ' + this.apellido;
+    }
+  },
+  watch: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
